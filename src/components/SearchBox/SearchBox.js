296cdcd0b8650c31dd3b35924608015c8081309d
Aida IdriSova <aida-107@mail.ru>
2021-07-17 16:14:03 +0300

    project_4_4

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -9,30 +9,25 @@ class SearchBox extends Component {
         searchLine: '',
     };
    
-    // transliterate = (input, engToRus) => {
-    //     let eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g),
-    //         rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g)
-    //     for (let x = 0; x < rus.length; x++) {
-    //       input = input.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x])
-    //       input = input.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase())
-    //     }
-    //     return input
-    //   }
+    transliterate = (input, engToRus) => {
+        let eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g),
+            rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g)
+        for (let x = 0; x < rus.length; x++) {
+          input = input.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x])
+          input = input.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase())
+        }
+        return input
+      }
 
     searchLineChangeHandler = (e) => {
         let input = e.target.value
-           
+
         if (input) {
             input = this.transliterate(input)
           } else {
               input = this.transliterate(input, true)
         };
-      
-        kirillicaChange = (input) => {
-            transliterate(input)
-            return input
-        }
-
+        
         this.setState({ searchLine: input });
        // console.log(e.target.value)
     }
