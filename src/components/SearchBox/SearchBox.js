7529060bc2f92be8ace35f785906d9b47a81c86f
Aleksei Sokolov <aleksei.sokolov.85@mail.ru>
2021-07-17 12:49:30 +0300

    fixes with transliteration

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -7,34 +7,32 @@ class SearchBox extends Component {
     state = {
         searchLine: ''
     }
+    transliterate = (input, engToRus) => {
+        let eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g),
+            rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g)
+        for (let x = 0; x < rus.length; x++) {
+            input = input.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x])
+            input = input.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase())
+        }
+        return input
+    }
+
     searchLineChangeHandler = (e) => {
-        this.setState({ searchLine: e.target.value });
-        console.log(e.target.value)
+        let input = e.target.value
+
+        console.log('----', input)
+        if (input) {
+            input = this.transliterate(input)
+        } else {
+            input = this.transliterate(input, true)
+        }
+        console.log('----', input)
+        
+        this.setState({ searchLine: input });
     }
     searchBoxSubmitHandler = (e) => {
         e.preventDefault();
-        let input = e.target.choise.value
-
-        let transliterate = (input, engToRus) => {
-            let eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g),
-                rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g)
-            for (let x = 0; x < rus.length; x++) {
-              input = input.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x])
-              input = input.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase())
-            }
-            return input
-          }
-
-        if (e.target.choise.value) {
-            input = transliterate(input)
-          }
-          else {
-              input = transliterate(input, true)
-        }
-        console.log(input)
-        
- 
-          
+        let input = e.target.value
 
         fetch(`http://www.omdbapi.com/?s=${input}&apikey=341e2618 `)
            .then((res) => res.json())
